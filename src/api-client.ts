f414ad0ec7e0109530e681b9c4f28ce0e764b1eb
irinabolshakova <irina.bolshakov.ee@gmail.com>
2024-12-25 23:24:25 +0200

    Changed test files, updated api-client.ts and ci.yml files

--- src/api-client.ts
+++ src/api-client.ts
@@ -1,26 +1,59 @@
-import { APIRequestContext } from 'playwright'
-import {request} from "@playwright/test";
+import { APIRequestContext } from "playwright";
+import { response } from "express";
+import { APIResponse, expect, request } from "@playwright/test";
+import { StatusCodes } from "http-status-codes";
 
-let baseURL: string = 'http://localhost:3000/users';
+let baseURL: string = "http://localhost:3000/users";
 
 export class ApiClient {
-    static instance: ApiClient
-    private request: APIRequestContext
+  static instance: ApiClient;
+  private request: APIRequestContext;
 
-    private constructor(request: APIRequestContext) {
-        this.request = request
+  private constructor(request: APIRequestContext) {
+    this.request = request;
+  }
+  public static async getInstance(
+    request: APIRequestContext,
+  ): Promise<ApiClient> {
+    if (!ApiClient.instance) {
+      ApiClient.instance = new ApiClient(request);
     }
-    public static async getInstance(request: APIRequestContext): Promise<ApiClient> {
-        if (!ApiClient.instance) {
-            ApiClient.instance = new ApiClient(request)
-        }
-        return ApiClient.instance
-    }
+    return ApiClient.instance;
+  }
 
-    async createUsers(users: number): Promise<number> {
-        for (let i = 0; i < users; i++) {
-            const response = await this.request.post(baseURL)
-        }
-        return users
+  async createUsers(users: number): Promise<number> {
+    for (let i = 0; i < users; i++) {
+      const responseUsers = await this.request.post(baseURL);
     }
+    return users;
+  }
+
+  async getUserByUserId(userID: number): Promise<APIResponse> {
+    const response = await this.request.get(`${baseURL}/${userID}`);
+    return response;
+  }
+
+  async deleteUsers(userID: number): Promise<any> {
+    const response = await this.request.get(baseURL);
+    const responseBody = await response.json();
+    const numberOfObjects = responseBody.length;
+    let userIDs = [];
+    for (let i = 0; i < numberOfObjects; i++) {
+      let userID = responseBody[i].id;
+      userIDs.push(userID);
+    }
+    for (let i = 0; i < numberOfObjects; i++) {
+      let response = await this.request.delete(`${baseURL}/${userIDs[i]}`);
+    }
+  }
+
+  async checkAllUsersDeleted(): Promise<void> {
+    const responseAfterDelete = await this.request.get(baseURL);
+    expect(responseAfterDelete.status()).toBe(StatusCodes.OK);
+    const responseBodyEmpty = await responseAfterDelete.text();
+    console.log("All users are deleted");
+    if (responseBodyEmpty != "[]") {
+      throw new Error("Not all users are deleted");
+    }
+  }
 }
--- tests/user-controller.test.ts
+++ tests/user-controller.test.ts
@@ -1,8 +1,8 @@
 // tests/api.spec.ts
 import { test, expect } from '@playwright/test';
 import {ApiClient} from "../src/api-client";
+import {StatusCodes} from "http-status-codes";
 let baseURL: string = 'http://localhost:3000/users';
-
 let userID: number;
 
 //test.beforeAll(async ({ request }) => {
@@ -34,26 +34,59 @@ test.beforeEach(async ({ request }) => {
         // delete user by id
         let response = await request.delete(`${baseURL}/${userIDs[i]}`);
         // validate the response status code
-        expect.soft(response.status()).toBe(200);
+        expect.soft(response.status()).toBe(StatusCodes.OK);
     }
 
     // verify that all users are deleted
     const responseAfterDelete = await request.get(`${baseURL}`);
-    expect(responseAfterDelete.status()).toBe(200);
+    expect(responseAfterDelete.status()).toBe(StatusCodes.OK);
     const responseBodyEmpty = await responseAfterDelete.text()
     // validate that the response is an empty array
     expect(responseBodyEmpty).toBe('[]');
 })
 
 test.describe('User management API', () => {
+    test.beforeEach(async ({ request }) => {
+        // get all users
+        const response = await request.get(`${baseURL}`);
+        const responseBody = await response.json()
+        // get the number of objects in the array returned
+        const numberOfObjects = responseBody.length;
+
+        // create an empty array to store all user ID
+        let userIDs = [];
+
+        // loop through all users and store their ID in an array
+        for (let i = 0; i < numberOfObjects; i++) {
+            // get user ID from the response
+            let userID = responseBody[i].id;
+            // push is used to add elements to the end of an array
+            userIDs.push(userID);
+        }
+
+        // delete all users in a loop using previously created array
+        for (let i = 0; i < numberOfObjects; i++) {
+            // delete user by id
+            let response = await request.delete(`${baseURL}/${userIDs[i]}`);
+            // validate the response status code
+            expect.soft(response.status()).toBe(StatusCodes.OK);
+        }
+
+        // verify that all users are deleted
+        const responseAfterDelete = await request.get(`${baseURL}`);
+        expect(responseAfterDelete.status()).toBe(StatusCodes.OK);
+        const responseBodyEmpty = await responseAfterDelete.text()
+        // validate that the response is an empty array
+        expect(responseBodyEmpty).toBe('[]');
+    })
     test('GET / - should return empty when no users', async ({ request }) => {
         const response = await request.get(`${baseURL}`);
-        expect(response.status()).toBe(200);
+        expect(response.status()).toBe(StatusCodes.OK);
         const responseBody = await response.text()
-        expect(response.status()).toBe('[]');
+        expect(response.status()).toBe(StatusCodes.OK);
     });
 
-    test('POST/ - should return empty when no users', async ({ request }) => {
+    test('POST/ - create n users', async ({ request }) => {
         const apiClient = await ApiClient.getInstance(request)
         const usersCount = await apiClient.createUsers(5)
         const response = await request.get(`${baseURL}`);
@@ -90,22 +123,50 @@ test.describe('User management API', () => {
             expect(expResponseBody).toStrictEqual([])
         });
 
+    test('DELETE/ - delete m users',
+        async ({request}) => {
+            const apiClient = await ApiClient.getInstance(request)
+            const usersCount = await apiClient.createUsers(5)
+            let userIDs = [];
+            const response = await request.get(`${baseURL}`);
+            const responseBody = await response.json()
+
+            // get the number of objects in the array returned
+
+            const numberOfObjects = responseBody.length;
+            // loop through all users and store their ID in an array
+            for (let i = 0; i < usersCount; i++) {
+                // get user ID from the response
+                let userID = responseBody[i].id;
+                // push is used to add elements to the end of an array
+                userIDs.push(userID);
+            }
+            for (let i = 0; i < 4; i++) {
+                // delete user by id
+                let response = await request.delete(`${baseURL}/${userIDs[i]}`);
+            }
+            const expResponse = await request.get(`${baseURL}`);
+            const expResponseBody = await expResponse.json()
+            let numberOfUsers = expResponseBody.length
+            expect(numberOfUsers).toBe(usersCount-4)
+        });
+
     test('GET /:id - should return a user by ID', async ({ request }) => {
         const response = await request.get(`${baseURL}`+'/'+ userID)
         const responseBody = await response.text()
         console.log(responseBody)
-        expect(response.status()).toBe(200)
+        expect(response.status()).toBe(StatusCodes.OK)
     });
 
     test('GET /:id - should return 404 if user not found', async ({ request }) => {
         const response = await request.get(`${baseURL}`+'/'+9999999)
-        expect(response.status()).toBe(404)
+        expect(response.status()).toBe(StatusCodes.NOT_FOUND)
     });
 
     test('POST / - should add a new user', async ({ request }) => {
         const response = await request.post(`${baseURL}`);
         const body = await response.json();
-        expect.soft(response.status()).toBe(201)
+        expect.soft(response.status()).toBe(StatusCodes.CREATED)
         expect.soft(body.id).toBeDefined()
 
     });
@@ -114,7 +175,7 @@ test.describe('User management API', () => {
         const response = await request.delete(`${baseURL}`+'/'+userID)
         const responseBody = await response.json()
         console.log(responseBody)
-        expect.soft(response.status()).toBe(200)
+        expect.soft(response.status()).toBe(StatusCodes.OK)
         expect.soft(responseBody[0].Id).toBe(userID)
     });
 
@@ -122,8 +183,6 @@ test.describe('User management API', () => {
         const response = await request.delete(`${baseURL}`+'/'+'1')
         const responseBody = await response.json()
         console.log(responseBody)
-        expect.soft(response.status()).toBe(404)
+        expect.soft(response.status()).toBe(StatusCodes.NOT_FOUND)
     });
-
-
 });
